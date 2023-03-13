fb5caec968beb078475d777ed2095765665406f2
Luke Francl <look@recursion.org>
2023-03-13 05:00:52 +0000

    Implement min_products

--- week2/createContentTrainingData.py
+++ week2/createContentTrainingData.py
@@ -61,9 +61,22 @@ def _label_filename(filename):
 if __name__ == '__main__':
     files = glob.glob(f'{directory}/*.xml')
     print("Writing results to %s" % output_file)
+    categories = {}
     with multiprocessing.Pool() as p:
         all_labels = tqdm(p.imap(_label_filename, files), total=len(files))
-        with open(output_file, 'w') as output:
-            for label_list in all_labels:
-                for (cat, name) in label_list:
-                    output.write(f'__label__{cat} {name}\n')
+
+        for label_list in all_labels:
+            for (cat, name) in label_list:
+                # TODO: Is this multiprocessing safe?
+                if cat not in categories:
+                    categories[cat] = [name]
+                else:
+                    categories[cat].append(name)
+            
+    with open(output_file, 'w') as output:
+        for cat, values in categories.items():
+            if min_products > 0 and len(values) < min_products:
+                continue
+
+            for value in values:
+                output.write(f'__label__{cat} {value}\n')
