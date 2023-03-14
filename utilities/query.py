ea86a725ffa2b42c2ebf6a494fb11180d3af200f
Luke Francl <look@recursion.org>
2023-03-14 06:42:50 +0000

    Add --synonyms option to query name.synonyms

--- utilities/query.py
+++ utilities/query.py
@@ -49,7 +49,7 @@ def create_prior_queries(doc_ids, doc_id_weights,
 
 
 # Hardcoded query here.  Better to use search templates or other query config.
-def create_query(user_query, click_prior_query, filters, sort="_score", sortDir="desc", size=10, source=None):
+def create_query(user_query, click_prior_query, filters, synonyms=False, sort="_score", sortDir="desc", size=10, source=None):
     query_obj = {
         'size': size,
         "sort": [
@@ -167,6 +167,17 @@ def create_query(user_query, click_prior_query, filters, sort="_score", sortDir=
             }
         }
     }
+
+    # Replace name match query with name.synonyms match query
+    if synonyms:
+        query_obj["query"]["function_score"]["query"]["bool"]["should"][0] = {
+            "match": {
+                "name.synonyms": {
+                    "query": user_query
+                }
+            }
+        }
+
     if click_prior_query is not None and click_prior_query != "":
         query_obj["query"]["function_score"]["query"]["bool"]["should"].append({
             "query_string": {
@@ -186,12 +197,13 @@ def create_query(user_query, click_prior_query, filters, sort="_score", sortDir=
     return query_obj
 
 
-def search(client, user_query, index="bbuy_products", sort="_score", sortDir="desc"):
+def search(client, user_query, index="bbuy_products", sort="_score", sortDir="desc", synonyms=False):
     #### W3: classify the query
     #### W3: create filters and boosts
     # Note: you may also want to modify the `create_query` method above
-    query_obj = create_query(user_query, click_prior_query=None, filters=None, sort=sort, sortDir=sortDir, source=["name", "shortDescription"])
+    query_obj = create_query(user_query, synonyms=synonyms, click_prior_query=None, filters=None, sort=sort, sortDir=sortDir, source=["name", "shortDescription"])
     logging.info(query_obj)
+    print(query_obj)
     response = client.search(query_obj, index=index)
     if response and response['hits']['hits'] and len(response['hits']['hits']) > 0:
         hits = response['hits']['hits']
@@ -212,6 +224,7 @@ if __name__ == "__main__":
                          help='The OpenSearch port')
     general.add_argument('--user',
                          help='The OpenSearch admin.  If this is set, the program will prompt for password too. If not set, use default of admin/admin')
+    general.add_argument('--synonyms', action='store_true', help='Query with synonyms')
 
     args = parser.parse_args()
 
@@ -239,14 +252,13 @@ if __name__ == "__main__":
 
     )
     index_name = args.index
-    query_prompt = "\nEnter your query (type 'Exit' to exit or hit ctrl-c):"
-    print(query_prompt)
-    for line in fileinput.input():
-        query = line.rstrip()
-        if query == "Exit":
-            break
-        search(client=opensearch, user_query=query, index=index_name)
 
-        print(query_prompt)
+    query_prompt = "\nEnter your query (type 'Exit' to exit or hit ctrl-c):"
+    while True:
+        line = input(query_prompt).rstrip()
+        query = line.rstrip()
+        if query.lower() == "exit":
+            exit(0)
+        search(client=opensearch, user_query=query, index=index_name, synonyms=args.synonyms)
 
     
